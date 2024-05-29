2b5f4de90393d421ddb0e2a7b6ec3c7479045a3d
AHQ Miness <ahqsecret@gmail.com>
2024-05-29 22:04:44 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-const { Octokit } = require("octokit");
-const data = require("./bot.node");
+(async () => {
+  const { Octokit } = await import("octokit");
+  const data = require("./bot.node");
 
-console.log(data);
+  console.log(data);
 
-const context = process.env["CTX"];
-console.log(context);
+  const context = process.env["CTX"];
+  console.log(context);
 
-const client = new Octokit({
-  auth: process.env["GH_TOKEN"],
-});
+  const client = new Octokit({
+    auth: process.env["GH_TOKEN"],
+  });
+})();
